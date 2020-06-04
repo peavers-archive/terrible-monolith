e36d6433ec4db7ecb46dfc87255a2c3da6de828f
Chris Turner <chris@codacy.com>
2020-06-04 14:43:11 +0100

    feature: Add task for recreating thumbnails

--- terrible-app/src/main/java/io/terrible/app/controller/MediaFileController.java
+++ terrible-app/src/main/java/io/terrible/app/controller/MediaFileController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+
 @Slf4j
 @CrossOrigin
 @RestController
---
+++ terrible-app/src/main/java/io/terrible/app/controller/TaskController.java
@@ -0,0 +1,33 @@
+package io.terrible.app.controller;
+
+import io.terrible.app.domain.MediaFile;
+import io.terrible.app.services.MediaFileService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+
+@Slf4j
+@CrossOrigin
+@RestController
+@RequestMapping("/task")
+@RequiredArgsConstructor
+public class TaskController {
+
+  private final MediaFileService mediaFileService;
+
+  @GetMapping("/recreate-thumbnails/{id}")
+  public Mono<MediaFile> invalidateThumbnails(@PathVariable final String id) {
+
+    return mediaFileService
+        .findById(id)
+        .doOnNext(mediaFile -> mediaFile.setThumbnails(new ArrayList<>(12)))
+        .flatMap(mediaFileService::save);
+  }
+}
